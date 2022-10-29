4389471f2058644ecd357aa804452e3af122e405
maxentr <rouaultmaxent@gmail.com>
2022-10-29 20:34:43 +0200

    feat(types): add drink type

---
+++ types/drink.type.ts
@@ -0,0 +1,17 @@
+export type DrinkStockSize = {
+  _id: string
+  name: string
+  price: number
+  quantity: number
+}
+
+export type Drink = {
+  _id: string
+  name: string
+  description: string
+  image?: string
+  stockLeft: number
+  size: DrinkStockSize[]
+  createdAt: Date
+  updatedAt: Date
+}
