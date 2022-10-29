6411ab070c69f8b5a409011d49ab9a9176c06655
maxentr <rouaultmaxent@gmail.com>
2022-10-29 20:51:13 +0200

    feat(admin): add Ingredients management

---
+++ components/pages/admin/IngredientSlideOver.tsx
@@ -0,0 +1,203 @@
+import { PlusIcon } from "@heroicons/react/20/solid"
+import { useEffect, useReducer, useState } from "react"
+import { useToast } from "../../../hooks/contexts/useToast"
+import {
+  addIngredient,
+  editIngredient,
+  getIngredientsStockType,
+} from "../../../services/ingredient.service"
+import { Ingredient } from "../../../types/ingredient.type"
+import SlideOver from "../../ui/SlideOver"
+
+export type DrinkSlideOverProps = {
+  toggle: () => void
+  isShowing: boolean
+  ingredient: Ingredient | undefined
+  // eslint-disable-next-line no-unused-vars
+  close: (type: "create" | "edit", inegredient: Ingredient) => void
+}
+
+interface Action {
+  name: string
+  value: string
+}
+
+const formReducer = (state: Object, action: Action) => {
+  const { name, value } = action
+  return {
+    ...state,
+    [name]: value,
+  }
+}
+
+const IngredientSlideOver = ({
+  toggle,
+  isShowing,
+  ingredient,
+  close,
+}: DrinkSlideOverProps) => {
+  const { addToast } = useToast()
+  const [formData, dispatch] = useReducer(formReducer, { ...ingredient })
+  const [stockTypes, setStockTypes] = useState<string[]>([])
+
+  useEffect(() => {
+    const getAndSetStockTypes = async () => {
+      const response = await getIngredientsStockType()
+      setStockTypes(response)
+    }
+    getAndSetStockTypes()
+  }, [])
+
+  const submitIngredient = async () => {
+    // Flemme de faire un vrai form
+    if (!formData) return
+
+    if (ingredient) {
+      const response = await editIngredient(
+        ingredient._id,
+        formData as Ingredient,
+      )
+      if (response) {
+        addToast({
+          type: "success",
+          title: "Succès",
+          message: "L'ingrédient a bien été modifié",
+        })
+        close("edit", response)
+      }
+    } else {
+      const response = await addIngredient(formData as Ingredient)
+      if (response) {
+        addToast({
+          type: "success",
+          title: "Succès",
+          message: "L'ingrédient a bien été ajouté",
+        })
+        close("create", response)
+      }
+    }
+  }
+
+  return (
+    <SlideOver
+      title={ingredient ? "Modifier l'ingredient" : "Ajouter un ingredient"}
+      toggle={toggle}
+      isShowing={isShowing}
+    >
+      <div className="flex flex-grow flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name">Nom de l&apos;ingredient :</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            defaultValue={ingredient?.name || ""}
+            className="border border-gray-900 rounded-md px-4 py-2"
+            onChange={(e) => {
+              dispatch({
+                name: e.target.name.toString(),
+                value: e.target.value.toString(),
+              })
+            }}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="description">Description :</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            defaultValue={ingredient?.description || ""}
+            className="border border-gray-900 rounded-md px-4 py-2"
+            onChange={(e) => {
+              dispatch({
+                name: e.target.name.toString(),
+                value: e.target.value.toString(),
+              })
+            }}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="stockLeft">Quantité en stock :</label>
+          <input
+            type="number"
+            id="stockLeft"
+            name="stockLeft"
+            defaultValue={+(ingredient?.stockLeft || 0)}
+            className="border border-gray-900 rounded-md px-4 py-2"
+            onChange={(e) => {
+              dispatch({
+                name: e.target.name.toString(),
+                value: e.target.value.toString(),
+              })
+            }}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="stockType">Unité de stockage :</label>
+          <select
+            name="stockType"
+            id="stockType"
+            className="border border-gray-900 rounded-md px-4 py-2 bg-white h-[42px]"
+            defaultValue={ingredient?.stockType && ingredient?.stockType}
+            onChange={(e) => {
+              dispatch({
+                name: e.target.name.toString(),
+                value: e.target.value.toString(),
+              })
+            }}
+          >
+            {stockTypes.map((stockType) => (
+              <option key={stockType} value={stockType}>
+                {stockType}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="price">Prix en € :</label>
+          <input
+            type="number"
+            id="price"
+            name="price"
+            defaultValue={+(ingredient?.price || 0)}
+            className="border border-gray-900 rounded-md px-4 py-2"
+            onChange={(e) => {
+              dispatch({
+                name: e.target.name.toString(),
+                value: e.target.value.toString(),
+              })
+            }}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="category">Categorie :</label>
+          <input
+            type="text"
+            id="category"
+            name="category"
+            defaultValue={ingredient?.category || ""}
+            className="border border-gray-900 rounded-md px-4 py-2"
+            onChange={(e) => {
+              dispatch({
+                name: e.target.name.toString(),
+                value: e.target.value.toString(),
+              })
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex flex-row justify-end items-center">
+        <button
+          onClick={submitIngredient}
+          className="bg-gray-900 active:bg-black text-white px-4 py-2 rounded-md flex items-center justify-center gap-2"
+        >
+          {!ingredient && <PlusIcon className="fill-white w-4 h-4" />}
+          <p>{ingredient ? "Modifier" : "Ajouter"} l&apos;ingredient</p>
+        </button>
+      </div>
+    </SlideOver>
+  )
+}
+
+export default IngredientSlideOver
---
+++ pages/admin/Ingredients.tsx
@@ -0,0 +1,185 @@
+import { useEffect, useState } from "react"
+import AdminLayout from "../../layouts/AdminLayout"
+import { Ingredient, StockType } from "../../types/ingredient.type"
+import {
+  deleteIngredient,
+  getIngredients,
+} from "../../services/ingredient.service"
+import RCTable from "rc-table"
+import { PencilIcon } from "@heroicons/react/24/solid"
+import { TrashIcon } from "@heroicons/react/20/solid"
+import { ColumnsType, DefaultRecordType } from "rc-table/lib/interface"
+import { useToast } from "../../hooks/contexts/useToast"
+import IngredientSlideOver from "../../components/pages/admin/IngredientSlideOver"
+import useModal from "../../hooks/useModal"
+
+const Ingredients = () => {
+  const { addToast } = useToast()
+  const { isShowing, toggle } = useModal()
+  const [ingredients, setIngredients] = useState<Ingredient[]>([])
+  const [selectedIngredient, setSelectedIngredient] = useState<Ingredient>()
+
+  useEffect(() => {
+    const getAndSetIngredients = async () => {
+      const response = await getIngredients()
+      setIngredients(response)
+    }
+    getAndSetIngredients()
+  }, [])
+
+  const columns: ColumnsType<DefaultRecordType> = [
+    {
+      title: "Nom",
+      dataIndex: "name",
+      key: "name",
+      className: "text-left px-4 border-l border-y border-gray-200 py-2",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+      className: "text-left px-4 border-y border-gray-200 py-2",
+    },
+    {
+      title: "Prix",
+      render: (row: Ingredient) => `${row.price} €`,
+      key: "price",
+      className: "text-left px-4 border-y border-gray-200 py-2",
+    },
+    {
+      title: "Quantité en stock",
+      render: (row: Ingredient) => `${row.stockLeft}`,
+      key: "stockLeft",
+      className: "text-left px-4 border-y border-gray-200 py-2",
+    },
+    {
+      title: "Type de stock",
+      render: (row: Ingredient) =>
+        `${
+          row.stockType === StockType.KILOGRAMS
+            ? "gramme"
+            : row.stockType === StockType.LITERS
+            ? "litre"
+            : "unité"
+        }`,
+      key: "stockType",
+      className: "text-left px-4 border-y border-gray-200 py-2",
+    },
+    {
+      title: "Modifier",
+      render: () => (
+        <div className="h-full w-full flex justify-end">
+          <PencilIcon className="w-5 h-5 fill-gray-500 cursor-pointer" />
+        </div>
+      ),
+      key: "createdAt",
+      width: 50,
+      onCell: (record) => ({
+        onClick() {
+          onClickHandler("edit", record as Ingredient)
+        },
+      }),
+      className: "text-right px-4 border-y border-gray-200 py-2",
+    },
+    {
+      title: "Supprimer",
+      render: () => (
+        <div className="h-full w-full flex justify-end">
+          <TrashIcon className="w-5 h-5 fill-red-500 cursor-pointer" />
+        </div>
+      ),
+      onCell: (record) => ({
+        onClick() {
+          onClickHandler("delete", record as Ingredient)
+        },
+      }),
+      key: "updatedAt",
+      width: 50,
+      className: "px-4 border-r border-y border-gray-200 py-2",
+    },
+  ]
+
+  const createIngredient = () => {
+    setSelectedIngredient(undefined)
+    toggle()
+  }
+
+  const onClickHandler = async (columnName: string, row: Ingredient) => {
+    if (columnName === "delete") {
+      await deleteIngredient(row._id)
+      const newIngredients = ingredients.filter(
+        (ingredient) => ingredient._id !== row._id,
+      )
+      setIngredients(newIngredients)
+
+      addToast({
+        title: "Suppression réussie",
+        message: "L'ingrédient a bien été supprimé",
+        type: "success",
+      })
+    } else if (columnName === "edit") {
+      setSelectedIngredient(row)
+      toggle()
+    }
+  }
+
+  const handleChangement = (
+    type: "create" | "edit",
+    ingredient: Ingredient,
+  ) => {
+    if (type === "create") {
+      setIngredients([...ingredients, ingredient])
+    } else if (type === "edit") {
+      const newIngredients = ingredients.map((ing) => {
+        if (ing._id === ingredient._id) {
+          return ingredient
+        }
+        return ing
+      })
+      setIngredients(newIngredients)
+    }
+    toggle()
+  }
+
+  return (
+    <AdminLayout>
+      <div className="flex flex-col items-center justify-center self-stretch w-full mt-8 mb-12">
+        <div className="max-w-7xl">
+          {ingredients.length > 0 && (
+            <RCTable
+              rowKey={(row) => row._id}
+              columns={columns}
+              expandable={{
+                defaultExpandAllRows: false,
+                expandedRowRender: (row) => (
+                  <p style={{ margin: 0 }}>{JSON.stringify(row)}</p>
+                ),
+                rowExpandable: () => true,
+                showExpandColumn: true,
+              }}
+              data={ingredients}
+              className=""
+              rowClassName=""
+            />
+          )}
+          <div className="w-full flex flex-row mt-4">
+            <button
+              className="bg-gray-900 text-white font-bold py-2 px-4 rounded"
+              onClick={createIngredient}
+            >
+              Ajouter un ingrédient
+            </button>
+          </div>
+        </div>
+        <IngredientSlideOver
+          isShowing={isShowing}
+          toggle={toggle}
+          ingredient={selectedIngredient}
+          close={handleChangement}
+        />
+      </div>
+    </AdminLayout>
+  )
+}
+
+export default Ingredients
