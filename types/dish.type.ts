b015f8d5de0cf0be117467ccf47d057d6e7de79f
maxentr <rouaultmaxent@gmail.com>
2022-10-29 20:34:35 +0200

    feat(types): add dish type

---
+++ types/dish.type.ts
@@ -0,0 +1,17 @@
+export type DishIngredient = {
+  _id: string;
+  id: string;
+  quantity: number;
+};
+
+export type Dish = {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  image?: string;
+  category?: string;
+  ingredients: DishIngredient[];
+  createdAt: Date;
+  updatedAt: Date;
+};
