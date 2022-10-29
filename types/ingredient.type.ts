8b5411da2e44c58f89509972483aa1e604241791
maxentr <rouaultmaxent@gmail.com>
2022-10-29 20:35:09 +0200

    feat(types): add ingredient type

---
+++ types/ingredient.type.ts
@@ -0,0 +1,17 @@
+export enum StockType {
+  LITERS = "LITERS",
+  KILOGRAMS = "KILOGRAMS",
+  UNITS = "UNITS",
+}
+
+export type Ingredient = {
+  _id: string
+  name: string
+  description?: string
+  category: string // Potential future id of categories collection
+  stockLeft: number
+  stockType?: StockType
+  price: number
+  createdAt: Date
+  updatedAt: Date
+}
