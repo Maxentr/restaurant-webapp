4dea2a58e7d219f64971e5b8aeaae41c4994e18c
maxentr <rouaultmaxent@gmail.com>
2022-10-31 23:14:40 +0100

    feat(hooks context): add useConfirmationModal

---
+++ hooks/contexts/useConfirmationModal.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext, useRef, useState } from "react"
+import Modal from "../../components/ui/Modal"
+import useModal from "../useModal"
+
+type ModalContent = {
+  title: string
+  message: string
+}
+
+type ModalContextType = {
+  showConfirmation: (title: string, message: string) => Promise<boolean>
+}
+
+type ConfirmationModalProviderProps = {
+  children: React.ReactNode
+}
+
+const ConfirmationModalContext = React.createContext<ModalContextType>(
+  {} as ModalContextType,
+)
+
+const ConfirmationModalProvider = (props: ConfirmationModalProviderProps) => {
+  const { isShowing, toggle } = useModal()
+  const [content, setContent] = useState<ModalContent | null>()
+  const resolver = useRef<Function>()
+
+  const handleShow = (title: string, message: string): Promise<boolean> => {
+    setContent({
+      title,
+      message,
+    })
+    toggle()
+    return new Promise(function (resolve) {
+      resolver.current = resolve
+    })
+  }
+
+  const modalContext: ModalContextType = {
+    showConfirmation: handleShow,
+  }
+
+  const handleOk = () => {
+    resolver.current && resolver.current(true)
+    toggle()
+  }
+
+  const handleCancel = () => {
+    resolver.current && resolver.current(false)
+    toggle()
+  }
+
+  return (
+    <ConfirmationModalContext.Provider value={modalContext}>
+      {props.children}
+
+      {content && (
+        <Modal
+          isShowing={isShowing}
+          toggle={toggle}
+          title={content.title}
+          message={content.message}
+          cancelBtnText="Annuler"
+          confirmBtnTxt="Supprimer"
+          onCancel={handleCancel}
+          onConfirm={handleOk}
+        />
+      )}
+    </ConfirmationModalContext.Provider>
+  )
+}
+
+export const useConfirmationModal = (): ModalContextType =>
+  useContext(ConfirmationModalContext)
+export default ConfirmationModalProvider
