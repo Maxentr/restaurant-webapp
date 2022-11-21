ff597e8bb553ca575f73ecc5f09a1cf5041de779
maxentr <rouaultmaxent@gmail.com>
2022-11-21 20:55:29 +0100

    refactor(Login): add toasts

--- app/login/Login.tsx
+++ app/login/Login.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import { HomeIcon } from "@heroicons/react/20/solid"
 import { useRouter } from "next/navigation"
 import React, { useState } from "react"
 import { useAuth } from "../../hooks/contexts/useAuth"
+import { useToast } from "../../hooks/contexts/useToast"
 import { login } from "../../services/auth.service"
 
 const Login = () => {
   const router = useRouter()
   const { newAcessToken } = useAuth()
+  const { addToast } = useToast()
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -16,8 +19,18 @@ const Login = () => {
     const response = await login(email, password)
     if ("error" in response) {
       console.log(response?.error)
+      addToast({
+        title: "Erreur",
+        message: response?.error.message as string,
+        type: "error",
+      })
     } else {
       newAcessToken(response?.data?.accessToken)
+      addToast({
+        title: "Succès",
+        message: "Vous vous êtes connecté avec succès",
+        type: "success",
+      })
       router.push("/admin")
     }
   }
@@ -30,12 +43,13 @@ const Login = () => {
           onClick={() => router.replace("/")}
           className="flex flex-row gap-2 items-center cursor-pointer"
         >
+          <HomeIcon className="fill-white w-5 h-5" />
           <p className="text-white font-medium">
-            Retourné à la page d&apos;accueil
+            Retourner à la page d&apos;accueil
           </p>
         </div>
       </nav>
-      <div className="flex flex-col flex-1 h-max justify-center items-center gap-8 bg-gray-50">
+      <div className="flex flex-col flex-1 h-[calc(100%-64px)] justify-center items-center gap-8 bg-gray-50">
         <h1 className="text-3xl font-medium">Connexion</h1>
         <input
           type="email"
