6b6ce50e98ecc8a6c2f27f6e60f04c80ad3a2354
maxentr <rouaultmaxent@gmail.com>
2022-11-23 20:25:47 +0100

    feat(order service): create order service

---
+++ services/order.service.ts
@@ -0,0 +1,48 @@
+import axios from "axios"
+import { ApiResponse } from "../types/apiResponse.type"
+import { ObjectId } from "../types/common.type"
+import { Order } from "../types/order.type"
+import { AxiosAuthConfig, AxiosErrorHandler } from "../utils/axiosHelper"
+
+const API_ROUTE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/orders`
+
+export const addOrder = async (menu: Order): ApiResponse<Order> => {
+  try {
+    const { data } = await axios.post(API_ROUTE_URL, menu, AxiosAuthConfig)
+    return data
+  } catch (e) {
+    return AxiosErrorHandler(e)
+  }
+}
+
+export const getOrders = async (): ApiResponse<Order[]> => {
+  try {
+    const { data } = await axios.get(API_ROUTE_URL, AxiosAuthConfig)
+    return data
+  } catch (e) {
+    return AxiosErrorHandler(e)
+  }
+}
+
+export const getOrdersByCustomer = async (
+  customerId: ObjectId,
+): ApiResponse<Order> => {
+  try {
+    const { data } = await axios.get(
+      `${API_ROUTE_URL}/customer/${customerId}`,
+      AxiosAuthConfig,
+    )
+    return data
+  } catch (e) {
+    return AxiosErrorHandler(e)
+  }
+}
+
+export const getOrder = async (id: ObjectId): ApiResponse<Order> => {
+  try {
+    const { data } = await axios.get(`${API_ROUTE_URL}/${id}`, AxiosAuthConfig)
+    return data
+  } catch (e) {
+    return AxiosErrorHandler(e)
+  }
+}
